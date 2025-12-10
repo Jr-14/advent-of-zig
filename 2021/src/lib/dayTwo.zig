8d91f2e2ab02993cde7921c910569b6c28fdc0d5
Jr-14 <jr.jemerson14@gmail.com>
2025-12-10 23:16:48 +1100

    Complete day 2 part 2 of 2021 AOC

--- 2021/src/lib/dayTwo.zig
+++ 2021/src/lib/dayTwo.zig
@@ -37,7 +37,48 @@ pub fn dayTwoPartOne() !u32 {
     return value;
 }
 
+pub fn dayTwoPartTwo() !i32 {
+    var line_reader = try LineReader.init(file_path);
+    defer line_reader.deinit();
+    
+    var horizontal_amount: i32 = 0;
+    var depth: i32 = 0;
+    var aim: i32 = 0;
+
+    while (try line_reader.nextLine()) |line| {
+        var line_iter = std.mem.splitAny(u8, line, " ");
+
+        var direction: []const u8 = undefined;
+        var amount: i32 = undefined;
+        if (line_iter.next()) |n| {
+            direction = n;
+        }
+        if (line_iter.next()) |n| {
+            amount = try std.fmt.parseInt(i32, n, 10);
+        }
+
+        if (std.mem.eql(u8, direction, "down")) {
+            aim += amount;
+        } else if (std.mem.eql(u8, direction, "up")) {
+            aim -= amount;
+        } else { // forward
+            horizontal_amount += amount;
+            depth += (aim * amount);
+        }
+    }
+
+    const total = depth * horizontal_amount;
+
+    std.debug.print("{d}\n", .{ total });
+    return total;
+}
+
 test "AOC 2021 Day 2 - Part 1" {
     const value = try dayTwoPartOne();
     try expect(value == 1507611);
+}
+
+test "AOC 2021 Day 2 - Part 2" {
+    const value = try dayTwoPartTwo();
+    try expect(value == 1880593125);
 }
