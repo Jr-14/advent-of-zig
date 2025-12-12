12b6c6138e52350ac9986be62bd041f8d6b0f86e
Jr-14 <jr.jemerson14@gmail.com>
2025-12-13 10:52:57 +1100

    Actually completed day 3

--- 2021/src/lib/dayThree.zig
+++ 2021/src/lib/dayThree.zig
@@ -12,10 +12,8 @@ const SomeBinaryData = struct {
 
     pub fn incrementCount(self: *Self, binary_number: u8) void {
         if (binary_number == 0) {
-            std.debug.print("number is: {any} - adding zero\n", .{ binary_number });
             self.zeros += 1;
         } else {
-            std.debug.print("number is: {any} - adding one\n", .{ binary_number });
             self.ones += 1;
         }
     }
@@ -24,7 +22,7 @@ const SomeBinaryData = struct {
         std.debug.print("zeros: {d},  ones: {d}\n", .{ self.zeros, self.ones });
     }
 
-    pub fn emitMostFrequent(self: Self) u1 {
+    pub fn emitMostFrequent(self: Self) u8 {
         if (self.zeros > self.ones) {
             return 0;
         } else {
@@ -32,7 +30,7 @@ const SomeBinaryData = struct {
         }
     }
 
-    pub fn emitLeastFrequent(self: Self) u1 {
+    pub fn emitLeastFrequent(self: Self) u8 {
         if (self.zeros > self.ones) {
             return 1;
         } else {
@@ -41,16 +39,28 @@ const SomeBinaryData = struct {
     }
 };
 
-pub fn  dayThreePartOne() !void {
+fn arrayToInt(arr: *const [12]u8) u32 {
+    var i: u32 = 12;
+    var total: u32 = 0;
+    while (i > 0) {
+        i -= 1;
+        if (arr[i] > 0) {
+            total += std.math.pow(u32, 2, 11 - i);
+        }
+    }
+    return total;
+}
+
+pub fn  dayThreePartOne() !u32 {
     var line_reader = try LineReader.init(file_path);
     defer line_reader.deinit();
 
-    var gpa = std.heap.DebugAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    // var gpa = std.heap.DebugAllocator(.{}){};
+    // defer _ = gpa.deinit();
+    // const allocator = gpa.allocator();
 
-    var list = try std.ArrayList(u32).initCapacity(allocator, 1000);
-    defer list.deinit(allocator);
+    // var list = try std.ArrayList(u32).initCapacity(allocator, 1000);
+    // defer list.deinit(allocator);
 
     const ARRAY_LENGTH: usize = 12;
 
@@ -77,30 +87,27 @@ pub fn  dayThreePartOne() !void {
         }
     }
 
-    var most_frequent: [ARRAY_LENGTH]u1 = undefined;
-    var least_frequent: [ARRAY_LENGTH]u1 = undefined;
+    var most_frequent: [ARRAY_LENGTH]u8 = undefined;
+    var least_frequent: [ARRAY_LENGTH]u8 = undefined;
 
     for (some_binary_array, 0..) |el, index| {
         most_frequent[index] = el.emitMostFrequent();
         least_frequent[index] = el.emitLeastFrequent();
     }
 
-    std.debug.print("most: {any}\n", .{ most_frequent });
-    std.debug.print("least: {any}\n", .{ least_frequent });
+    // std.debug.print("most: {any}\n", .{ most_frequent });
+    // std.debug.print("least: {any}\n", .{ least_frequent });
 
-    // const most = std.mem.readPackedInt(u12, &most_frequent, 0, .big);
-    // const least = std.mem.readPackedInt(u12, &least_frequent, 0, .big);
+    const most: u32 = arrayToInt(&most_frequent);
+    const least: u32 = arrayToInt(&least_frequent);
+    const total_power: u32 = most * least;
 
-    // const most = std.mem.readInt(u16, &most_frequent, .big);
-    // const least = std.mem.readInt(u16, &least_frequent, .big);
+    std.debug.print("most: {any}, least: {any}, total: {any}\n", .{ most, least, total_power });
 
-    const most: u32 = @bitCast(most_frequent);
-    const least: u32 = @bitCast(least_frequent);
-
-    std.debug.print("{any}, {any}\n", .{ most, least });
+    return total_power;
 }
 
 test "day three part one" {
-    try dayThreePartOne();
-    try expect(749376 == 749376);
+    const value = try dayThreePartOne();
+    try expect(value == 749376);
 }
